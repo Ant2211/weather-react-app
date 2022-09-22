8801b2aec0d6a13982158057d9ef835acd1e0e88
Ant2211 <turpak85@gmail.com>
2022-09-22 17:02:00 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import "./weatherstyle.css";
 export default function App() {
     return (
         <div className = "App">
+            <h3> 🌎 Global Weather </h3>
             <Weather />
-        </div>
+            <a href='https://github.com/Ant2211/weather-react-app' target="_blank" rel="noreferrer"> Github link</a>        </div>
     )
 }
