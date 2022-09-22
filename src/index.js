b46b46b18ebc516bee4cb2da30ae4ffa6478a018
Ant2211 <turpak85@gmail.com>
2022-09-22 12:33:56 +0300

    added weather style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Weather from "./Weather.js"
 import reportWebVitals from './reportWebVitals';
+import "./weatherstyle.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
